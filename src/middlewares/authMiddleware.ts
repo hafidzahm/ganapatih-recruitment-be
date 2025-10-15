fcc43782ace151bd8d34481e7eaa0757f44ee340
Abdul Hafizh <hafizhahm123@gmail.com>
2025-10-15 10:58:34 +0700

    implement auth middleware

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,8 +1,39 @@
 import type { NextFunction, Response } from 'express';
-import type { CustomRequest } from '../types/customRequest.ts';
+import type { CustomRequest, UserPayload } from '../types/customRequest.ts';
+import JwtService from '../utils/jwt.ts';
 
 export default function authMiddleware(
   req: CustomRequest,
   res: Response,
   next: NextFunction,
-) {}
+) {
+  const authorization = req?.headers?.authorization;
+  //   console.log({ authorization });
+  if (!authorization) {
+    throw {
+      type: 'AuthenticationError',
+      message: 'Invalid token',
+    };
+  }
+
+  const [bearer, token] = authorization.split(' ');
+  if (bearer !== 'Bearer' || !token) {
+    throw {
+      type: 'AuthenticationError',
+      message: 'Invalid token',
+    };
+  }
+
+  const isVerified = JwtService.verify(token) as UserPayload;
+  if (!isVerified) {
+    throw {
+      type: 'AuthenticationError',
+      message: 'Invalid token',
+    };
+  }
+  console.log({ isVerified });
+
+  req.user = { id: isVerified.id, username: isVerified.username };
+
+  next();
+}
