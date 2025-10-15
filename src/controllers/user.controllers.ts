8c4beaad7a319247e7ef1e4c53edf47c32473147
Abdul Hafizh <hafizhahm123@gmail.com>
2025-10-15 07:39:41 +0700

    implement registerUser at user.controller

---
+++ src/controllers/user.controllers.ts
@@ -0,0 +1,18 @@
+import type { NextFunction, Request, Response } from 'express';
+import UserService from '../services/user.services.ts';
+
+class UserController {
+  static async registerUser(req: Request, res: Response, next: NextFunction) {
+    try {
+      const user = await UserService.registerUser(req.body);
+      return res.status(201).json({
+        id: user.id,
+        username: user.username,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+export default UserController;
