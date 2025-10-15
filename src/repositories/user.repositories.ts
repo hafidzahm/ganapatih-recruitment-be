efe265f30f5e7d646834138d55d6f5093b3407f4
Abdul Hafizh <hafizhahm123@gmail.com>
2025-10-15 07:30:14 +0700

    create register db logic in user.repo

--- src/repositories/user.repositories.ts
+++ src/repositories/user.repositories.ts
@@ -1,12 +1,14 @@
+import BcryptService from '../utils/bcrypt.ts';
 import { prisma } from '../utils/prisma.ts';
 import type { RegisterSchemaType } from '../utils/schemas/user.schemas.ts';
 
 class UserRepository {
   static async register(data: RegisterSchemaType) {
+    const hashedPassword = BcryptService.hashPassword(data.password);
     return await prisma.users.create({
       data: {
         username: data.username,
-        password_hash: data.password,
+        password_hash: hashedPassword,
       },
     });
   }
