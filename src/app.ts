193a29dea8d59ff686d65909a046bc8498936601
Abdul Hafizh <hafizhahm123@gmail.com>
2025-10-15 07:45:28 +0700

    implement body parser middleware

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import router from './routes/index.ts';
+import bodyParser from 'body-parser';
 const app = express();
 
+app.use(bodyParser);
+// ------------------
 app.use(router);
 
 export default app;
