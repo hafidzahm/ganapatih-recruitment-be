0f60a39f1052216e66999ad45731b058971ebae4
Abdul Hafizh <hafizhahm123@gmail.com>
2025-10-15 09:19:30 +0700

    adding error handler

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -20,6 +20,21 @@ export default function errorHandler(
         message: 'Username already exists',
       });
     }
+
+    if (error.code === 'P2025') {
+      return res.status(404).json({
+        success: false,
+        message: 'Data not found',
+        details: 'No record was found',
+      });
+    }
+  }
+
+  if (error.type === 'AuthenticationError') {
+    return res.status(401).json({
+      success: false,
+      message: error.message,
+    });
   }
 
   if (error.type === 'ZodValidationError') {
