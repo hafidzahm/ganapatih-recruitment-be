51e3d22ade84bb306939416969a3accb04da83f9
Abdul Hafizh <hafizhahm123@gmail.com>
2025-10-15 16:41:26 +0700

    refactor delete and create follow

--- src/controllers/follow.controllers.ts
+++ src/controllers/follow.controllers.ts
@@ -41,13 +41,84 @@ class FollowController {
     req: CustomRequest,
     res: Response,
     next: NextFunction,
-  ) {}
+  ) {
+    try {
+      const loginId = req?.user?.id as string;
+      const targetId = req?.params?.userid as string;
+      if (loginId === targetId) {
+        throw {
+          type: 'BadRequest',
+          message: 'userId (actor) and userId (target) must different',
+        };
+      }
+      const targetUser = UserService.findUserById(targetId);
+      if (!targetUser) {
+        throw {
+          type: 'NotFound',
+          message: 'User not found',
+        };
+      }
+      const findedFollowData = await FollowService.findFollow(
+        loginId,
+        targetId,
+      );
+      let message: string;
+
+      //jika data follow tidak ditemukan
+      console.log({ findedFollowData });
+
+      if (!findedFollowData) {
+        await FollowService.createFollow(loginId, targetId);
+        message = `You are now following user ${targetId}`;
+      } else {
+        message = `You have following user ${targetId}`;
+      }
+
+      return res.status(200).json({
+        message,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
 
   static async deleteFollow(
     req: CustomRequest,
     res: Response,
     next: NextFunction,
-  ) {}
+  ) {
+    const loginId = req?.user?.id as string;
+    const targetId = req?.params?.userid as string;
+    const findedFollowData = await FollowService.findFollow(loginId, targetId);
+    if (loginId === targetId) {
+      throw {
+        type: 'BadRequest',
+        message: 'userId (actor) and userId (target) must different',
+      };
+    }
+    const targetUser = UserService.findUserById(targetId);
+    if (!targetUser) {
+      throw {
+        type: 'NotFound',
+        message: 'User not found',
+      };
+    }
+    let message: string;
+
+    //jika data follow ditemukan
+    console.log({ findedFollowData });
+
+    if (findedFollowData) {
+      await FollowService.deleteFollow(loginId, targetId);
+      message = `You unfollowing user ${targetId}`;
+    } else {
+      message = `You have unfollowed user ${targetId}`;
+    }
+
+    return res.status(200).json({
+      message,
+    });
+  }
 }
 
 export default FollowController;
