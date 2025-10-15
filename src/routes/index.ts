e87252b60dd14e4d7b0697e7cf33964922d89b52
Abdul Hafizh <hafizhahm123@gmail.com>
2025-10-15 13:11:08 +0700

    implement followRoutes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import homeRoute from './home.route.ts';
 import userRoutes from './user.route.ts';
 import postRoutes from './post.route.ts';
+import followRoutes from './follow.route.ts';
 
 const router = Router();
 
 router.use(homeRoute);
 router.use('/api', userRoutes);
 router.use('/api', postRoutes);
+router.use('/api', followRoutes);
 
 export default router;
