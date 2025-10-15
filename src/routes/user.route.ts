5a5cb3575872d342c449a1fc87805135b3988f0a
Abdul Hafizh <hafizhahm123@gmail.com>
2025-10-15 17:02:38 +0700

    refactor route naming

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -4,7 +4,7 @@ import UserController from '../controllers/user.controllers.ts';
 const userRoutes = Router();
 
 userRoutes.get('/users', UserController.getAll);
-userRoutes.post('/users', UserController.register);
-userRoutes.post('/sign-in', UserController.login);
+userRoutes.post('/register', UserController.register);
+userRoutes.post('/login', UserController.login);
 
 export default userRoutes;
