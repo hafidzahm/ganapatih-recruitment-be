579be19928e12b0dd0742f2cf54906af4ebbed80
Abdul Hafizh <hafizhahm123@gmail.com>
2025-10-15 16:10:21 +0700

    implement create, delete, and find follow data

--- src/services/follow.service.ts
+++ src/services/follow.service.ts
@@ -5,6 +5,21 @@ class FollowService {
     const follow = await FollowRepository.update(loginId, targetId);
     return follow;
   }
+
+  static async createFollow(loginId: string, targetId: string) {
+    const follow = await FollowRepository.create(loginId, targetId);
+    return follow;
+  }
+
+  static async deleteFollow(loginId: string, targetId: string) {
+    const follow = await FollowRepository.delete(loginId, targetId);
+    return follow;
+  }
+
+  static async findFollow(loginId: string, targetId: string) {
+    const follow = await FollowRepository.find(loginId, targetId);
+    return follow;
+  }
 }
 
 export default FollowService;
