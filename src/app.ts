e946d38752c6213bd9e851bbf3efc3be4ecd6914
Abdul Hafizh <hafizhahm123@gmail.com>
2025-10-15 08:15:10 +0700

    create and implement error middleware

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import router from './routes/index.ts';
 import bodyParser from 'body-parser';
+import errorHandler from './middlewares/errorHandler.ts';
 const app = express();
 
-app.use(bodyParser);
+app.use(bodyParser.json());
 // ------------------
 app.use(router);
+// ------------------
+app.use(errorHandler);
 
 export default app;
---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,43 @@
+import { Prisma } from '@prisma/client';
+import type { NextFunction, Request, Response } from 'express';
+
+export default function errorHandler(
+  error: CustomError,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  console.log('==errorHandler==');
+  console.log({ error });
+
+  if (error instanceof Prisma.PrismaClientKnownRequestError) {
+    console.log(error.code, 'errCode');
+
+    if (error.code === 'P2002') {
+      return res.status(409).json({
+        success: false,
+        message: 'Username already exists',
+      });
+    }
+  }
+
+  if (error.type === 'ZodValidationError') {
+    return res.status(400).json({
+      success: false,
+      message: error.message,
+      details: error.details,
+    });
+  }
+
+  // Handle other error types or send a generic error response
+  return res.status(500).json({
+    success: false,
+    message: 'Internal Server Error',
+  });
+}
+
+interface CustomError extends Error {
+  type: string;
+  message: string;
+  details?: string | unknown[]; // Zod errors typically provide an array of issues
+}
