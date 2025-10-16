de076666cb16a1039200e681f988427f122f6972
Abdul Hafizh <hafizhahm123@gmail.com>
2025-10-16 14:47:33 +0700

    install and implement cors

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,10 @@ import express from 'express';
 import router from './routes/index.ts';
 import bodyParser from 'body-parser';
 import errorHandler from './middlewares/errorHandler.ts';
+import cors from 'cors';
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 // ------------------
 app.use(router);
