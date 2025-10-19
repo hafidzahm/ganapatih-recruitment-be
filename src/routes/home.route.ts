3436958f99c710b63e185a9767ee178e327812fc
Abdul Hafizh <hafizhahm123@gmail.com>
2025-10-19 12:54:10 +0700

    add gitignore and refactor

--- src/routes/home.route.ts
+++ src/routes/home.route.ts
@@ -4,7 +4,7 @@ const homeRoute = Router();
 
 homeRoute.get('/', (req: Request, res: Response) => {
   return res.status(200).json({
-    message: 'Hello world!',
+    message: 'Ganapatih Recruitment API is LIVE!',
   });
 });
 
